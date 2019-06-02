d74a633f04cf5bd2901b35e03bfe90822d71a28e
Nathan Reyes <nathanreyes@me.com>
2019-06-01 19:28:20 -0500

    Add Github examples

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -11,6 +11,14 @@ export default ({ Vue }) => {
           L: 'YYYY-MM-DD', // Default for formatting/parsing dates
         },
       },
+      // Added for Github Issue #330
+      hu: {
+        firstDayOfWeek: 2,
+        masks: {
+          L: 'YYYY.MM.DD',
+          title: 'YYYY MMMM', // <- this doesn't work
+        },
+      },
     },
   });
 };
